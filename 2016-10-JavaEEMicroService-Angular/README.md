8b062c3f40023d242dcc66a79e900fd77448e9a9
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-02-23 12:13:20 +0100

    Adding Swagger

--- 2016-10-JavaEEMicroService-Angular/README.md
+++ 2016-10-JavaEEMicroService-Angular/README.md
@@ -29,22 +29,48 @@ Then create the project
 ```
 ng version
 ng new conference-web
+ng generate component venue
 
+```
+
+Execute it and go to [http://localhost:4200/]()
+
+```
+yarn run start
 ```
 
 ## Demo 03 - Docker
 
 ### Clean images
 
-* `docker images`
-* `/bin/bash -c 'docker rmi -f $(docker images -q --filter dangling=true)'`
-* `/bin/bash -c 'docker rmi -f $(docker images "agoncal/demo*" -q)'`
+* `docker image ls "agoncal/demo*"`
+* `docker image prune`
+* `/bin/bash -c 'docker image rm $(docker image ls "agoncal/demo*" -q) -f'`
 
 ### Build both Jar/War Docker images
 
 * `mvn clean package -Pdocker-war`
 * `mvn clean package -Pswarm,docker-jar`
 * Look at the image creation time and image size
-* `docker run -p8080:8080 <hash>`
-* `docker run -p9191:9191 <hash>`
+* `docker container run -p8080:8080 <hash war>`
+* `docker container run -p9191:9191 <hash jar>`
 * Change the repository class and re create the image
+
+## Demo 04 - Swagger and Angular Generation
+
+### Swagger
+
+* In `RoomEndpoint` show the Swagger annotations
+* In `pom.xml` show `swagger-maven-plugin`
+* Execute `mvn clean install -Pswarm`
+* Show under `webapp` the file `swagger.yaml`
+* Run Jar `java -jar target/demo01-conference-venue-swarm.jar`
+* Check URL `http://localhost:9191/demo01-conference-venue/swagger.json`
+
+### Generate Angular from Swagger
+
+/|\ Make sure to be under `demo01` directory
+
+* `swagger-codegen help`
+* `swagger-codegen generate -i http://localhost:9191/demo01-conference-venue/swagger.json -l typescript-angular2 -o conference-web/src/app/`
+* Show ` conference-web/src/app/api` and `model`
--- 2016-10-JavaEEMicroService-Angular/demo01/microservice-venue/src/main/java/org/agoncal/application/conference/venue/rest/RoomEndpoint.java
+++ 2016-10-JavaEEMicroService-Angular/demo01/microservice-venue/src/main/java/org/agoncal/application/conference/venue/rest/RoomEndpoint.java
@@ -1,5 +1,9 @@
 package org.agoncal.application.conference.venue.rest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.agoncal.application.conference.venue.domain.Room;
 import org.agoncal.application.conference.venue.repository.RoomRepository;
 
@@ -16,6 +20,7 @@ import java.util.List;
  *         --
  */
 @Path("/rooms")
+@Api(description = "Rooms REST Endpoint", tags = {"Venue"})
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -37,6 +42,10 @@ public class RoomEndpoint {
     // ======================================
 
     @POST
+    @ApiOperation(value = "Adds a new room to the venue")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid input")}
+    )
     public Response add(@NotNull Room room) {
         Room created = roomRepository.create(room);
         return Response.created(uriInfo.getAbsolutePathBuilder().path(created.getId()).build()).build();
@@ -44,6 +53,11 @@ public class RoomEndpoint {
 
     @GET
     @Path("/{id}")
+    @ApiOperation(value = "Finds a room by ID", response = Room.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid input"),
+            @ApiResponse(code = 404, message = "Room not found")}
+    )
     public Response retrieve(@PathParam("id") String id) {
 
         Room room = roomRepository.findById(id);
@@ -52,6 +66,10 @@ public class RoomEndpoint {
     }
 
     @GET
+    @ApiOperation(value = "Finds all the rooms", response = Room.class, responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Rooms not found")}
+    )
     public Response allRooms() {
         List<Room> allRooms = roomRepository.findAllRooms();
 
@@ -63,12 +81,20 @@ public class RoomEndpoint {
 
     @DELETE
     @Path("/{id}")
+    @ApiOperation(value = "Deletes a room")
+    @ApiResponses(value = {
+            @ApiResponse(code = 405, message = "Invalid room value")}
+    )
     public Response remove(@PathParam("id") String id) {
         roomRepository.delete(id);
         return Response.noContent().build();
     }
 
     @PUT
+    @ApiOperation(value = "Update an existing room")
+    @ApiResponses(value = {
+            @ApiResponse(code = 405, message = "Invalid values")}
+    )
     public Response update(@NotNull Room room) {
         roomRepository.update(room);
         return Response.ok(room).build();
